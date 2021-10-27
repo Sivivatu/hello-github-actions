091a17c7f17a7a9ff9239d1efde96a94a94c1c78
Paul Houghton <paul.j.houghton@gmail.com>
2021-10-27 13:41:18 +0100

    adding actions folder

---
+++ alteryx-github-actions/actions/alteryx_gallery.py
@@ -0,0 +1,9 @@
+import requests
+
+class Gallery:
+    def __init__(self, apiLocation, apiKey, apiSecret):
+        self.apiLocation = apiLocation
+        self.apiKey = apiKey
+        self.apiSecret = apiSecret
+
+    def getSubscriptionWorkflows():
